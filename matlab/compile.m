a8f9a6e031d03345d95ae6e6be6de629cbbe7f8f
Petrus1904 <49835604+Petrus1904@users.noreply.github.com>
2025-06-16 13:27:28 +0200

    Update compile.m
    compile now also sets the path

--- matlab/compile.m
+++ matlab/compile.m
@@ -5,3 +5,14 @@
 % (c) Peter Verheijen, 2025
 
 mex -R2018a superADMM_mex.c superADMM.c csparse.c ldl.c ccBlas.c -output superADMM -lmwblas -lmwlapack -DMATLAB_COMP
+
+thisFile = mfilename('fullpath');
+thisFolder = fileparts(thisFile);
+addpath(thisFolder);
+status = savepath;
+
+if status == 0
+    fprintf('superADMM compile completed.\n');
+else
+    fprintf('superADMM compile failed, try running again as Administrator,\n or add this folder manually to the default paths (Home->Environments->Set Path). \n');
+end
